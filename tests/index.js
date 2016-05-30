58e34cbc731cffb6bdefe13c4d22b3923f7767dd
Josh Klar <j@iv597.com>
2016-05-30 00:53:15 -0500

    Tests against other starting state stuff

--- tests/index.js
+++ tests/index.js
@@ -1,6 +1,9 @@
 import test from 'tape';
 
-import { createGame } from '../src/index';
+import {
+	createGame,
+	createPlayer,
+} from '../src/index';
 
 test('Base game initializes to what you get in a box', (t) => {
 	const gameName = 'Test Game';
@@ -14,3 +17,38 @@ test('Base game initializes to what you get in a box', (t) => {
 
 	t.end();
 });
+
+test('Players start with 4 unbuilt landmarks', (t) => {
+	const playerName = 'Test Player';
+	const { landmarks: lms } = createPlayer(playerName);
+
+	t.equal(lms.length, 4);
+
+	const built = [];
+	const unbuilt = [];
+
+	for (const lm of lms) {
+		(lm.built ? built : unbuilt).push(lm);
+	}
+
+	t.equal(built.length, 0);
+	t.equal(unbuilt.length, 4);
+
+	t.end();
+});
+
+test('Players start with a Wheat Field and Bakery as establishments', (t) => {
+	const playerName = 'Test Player';
+	const { establishments: es } = createPlayer(playerName);
+
+	t.equal(es.length, 2);
+
+	const wf = es.filter(e => e.title === 'Wheat Field');
+	const bk = es.filter(e => e.title === 'Bakery');
+
+	t.equal(wf.length, 1);
+	t.equal(bk.length, 1);
+
+	t.end();
+});
+
